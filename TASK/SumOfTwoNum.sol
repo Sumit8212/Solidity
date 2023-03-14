7d10712f0b1556213f90196d09d6859f3044dac0
sumit <rajputsumit543@gmail.com>
2023-03-14 12:25:30 +0530

    sumOfTwoNumber

---
+++ TASK/SumOfTwoNum.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract Sum{
+
+function sum(uint num1, uint num2)public pure returns(uint){
+    return num1+num2;
+}
+}
+
