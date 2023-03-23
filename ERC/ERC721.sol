52ad6a40a7d62058d399f2f8c024bbc085d649fb
sumit <rajputsumit543@gmail.com>
2023-03-23 10:36:46 +0530

    ERC721 mint&burn

--- ERC/ERC721.sol
+++ ERC/ERC721.sol
@@ -1,25 +1,14 @@
-// contracts/GameItem.sol
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract GameItem is ERC721URIStorage {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+contract MyToken is ERC721, ERC721Burnable, Ownable {
+    constructor() ERC721("MyToken", "MTK") {}
 
-    constructor() ERC721("GameItem", "ITM") {}
-
-    function awardItem(address player, string memory tokenURI)
-        public
-        returns (uint256)
-    {
-        uint256 newItemId = _tokenIds.current();
-        _mint(player, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-
-        _tokenIds.increment();
-        return newItemId;
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
     }
 }
