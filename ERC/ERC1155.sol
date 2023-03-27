d5d7de06e4159b630c9b87d93fa7797c10d184f1
sumit <rajputsumit543@gmail.com>
2023-03-27 16:01:21 +0530

    mint

---
+++ ERC/ERC1155.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract ERC1155{
+
+address public owner;
+
+constructor(){
+    owner=msg.sender;
+}
+
+mapping(address=>mapping(uint256 => uint256))balances;
+mapping(address => mapping(address => uint256))allowed;
+mapping(address => mapping(address => bool)) _operatorApprovals;
+
+
+ modifier onlyOwner(){
+        require(msg.sender == owner, "only owner can run this function");
+        _;
+    }
+
+    function mint(address account, uint256 id, uint256 amount)public onlyOwner{
+        balances[account][id]+=amount;
+    }
+    
+     
+
+}
